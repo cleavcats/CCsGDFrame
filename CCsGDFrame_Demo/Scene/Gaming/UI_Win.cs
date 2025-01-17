14238fc912623e8a5f35e3a4c5b4fe6a974bf2e0
变瞳的灵猫 <cleavcats@163.com>
2025-01-17 21:17:04 +0800

    Initial commit

---
+++ CCsGDFrame_Demo/Scene/Gaming/UI_Win.cs
@@ -0,0 +1,29 @@
+using CCsGDFrame;
+using Godot;
+using System;
+
+namespace Demo
+{
+    public class UI_Win : ColorRect
+    {
+        KeyBoardControler keyboard = new KeyBoardControler();
+        public override void _Ready()
+        {
+            this.FindNode("Win").Connect("pressed", this, "When_ButtonWin");
+        }
+        public override void _EnterTree()
+        {
+            // 用一个新的控制器 阻断 原本的键盘控制
+            GameSystem.keybord_controler.Add(keyboard);
+        }
+        public override void _ExitTree()
+        {
+            GameSystem.keybord_controler.Remove(keyboard);
+        }
+        void When_ButtonWin() 
+        {
+            GameSystem.ui_controler.UI_Close(this.Name);
+            GameSystem.scene_system.SceneJumpTo(GameSystem.root_node.StartScene); 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CCsGDFrame
+ 一个应用于 Godot 的 C# 小型游戏框架。
